e9e7b408c62456949d6bdbbc3f4218072f7fdc57
AndreuG01 <andreu.garcies01@estudiant.upf.edu>
2023-06-08 18:58:36 +0200

    some improvements with gpt2

--- src/segmentation.py
+++ src/segmentation.py
@@ -13,9 +13,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset, DataLoader
 
-# Step 1: Set up the environment
 
-# Step 2: Preprocess the data
 # Define the transformation to be applied to each image
 image_transform = transforms.Compose([
     transforms.Resize((224, 224)),  # Resize the image to a fixed size
@@ -23,7 +21,6 @@ image_transform = transforms.Compose([
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # Normalize the image
 ])
 
-# Step 3: Build the VGG model
 class VGG(nn.Module):
     def __init__(self):
         super(VGG, self).__init__()
@@ -36,47 +33,55 @@ class VGG(nn.Module):
         x = x.view(x.size(0), -1)
         return x
 
-# # Step 4: Build the language model
+
+
 class CaptionGenerator(nn.Module):
     def __init__(self):
         super(CaptionGenerator, self).__init__()
         self.vgg = VGG()
-        self.gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-        self.gpt2_tokenizer.add_special_tokens({'pad_token': self.gpt2_tokenizer.eos_token})
+        self.fc = nn.Linear(25088, 768) # TODO: try not to hard code
+        
         self.gpt2_model = GPT2LMHeadModel.from_pretrained('gpt2')
         self.gpt2_model.config.add_cross_attention = True
         # self.gpt2_model.config.return_dict = False
 
-    def forward(self, images, captions):
+    def forward(self, images, captions, tokenizer):
         # Step 1: Extract image features using VGG
         image_features = self.vgg(images)
 
+        prediction_outputs = []
         for image_feature, caption in zip(image_features, captions):
             # Step 2: Tokenize captions and prepare input tensors
-            inputs = self.gpt2_tokenizer(caption, return_tensors="pt", padding="max_length", truncation=True, max_length=50)
-            embeds = self.gpt2_model.transformer.wte.weight[inputs, :]
-            print(embeds)
+            # caption = self.gpt2_tokenizer(caption, return_tensors="pt")
+            embeds = self.gpt2_model.transformer.wte.weight[caption.input_ids, :]
             # Step 3: Pass image features through a feed-forward network to get a format suitable for GPT-2
             # Note: You might need to adjust the dimensions and other details according to your implementation.
-            #image_features = self.ffn(image_features.view(image_features.size(0), -1))
+            image_feature = self.fc(image_feature)
 
             # Step 4: Combine image features and caption tokens
             # We add the image features at the start of each sequence (assuming image_features are tensors of the appropriate size)
-            inputs["input_ids"] = torch.cat((image_feature, inputs["input_ids"]), dim=1)
-            inputs["attention_mask"] = torch.cat((torch.ones_like(image_feature), inputs["attention_mask"]), dim=1)
+            #inputs["input_ids"] = torch.cat((image_feature, inputs["input_ids"]), dim=1)
+            #inputs["attention_mask"] = torch.cat((torch.ones_like(image_feature), inputs["attention_mask"]), dim=1)
 
             # Step 5: Forward pass through GPT-2 model
-            outputs = self.gpt2_model(**inputs)
+            outputs = self.gpt2_model(inputs_embeds = embeds)
+            predicted_token_ids = torch.argmax(outputs.logits, dim=-1)
+            prediction_outputs.append(predicted_token_ids)
+            print(tokenizer.decode(predicted_token_ids[0]))
+            
 
-        return outputs
 
-# # Step 5: Prepare the data
+        return torch.tensor(prediction_outputs)
+
+
 class CocoDataset(Dataset):
     def __init__(self, image_dir, captions_file, transform=None):
         self.image_dir = image_dir
         self.coco = COCO(captions_file)
         self.image_ids = list(self.coco.imgs.keys())
         self.transform = transform
+        # self.gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
 
     def __len__(self):
         return len(self.image_ids)
@@ -90,8 +95,9 @@ class CocoDataset(Dataset):
             image = self.transform(image)
 
         caption_ids = self.coco.getAnnIds(image_id)
-        # caption = [self.coco.anns[cid]['caption'] for cid in caption_ids]
+        caption = [self.coco.anns[cid]['caption'] for cid in caption_ids]
         caption = self.coco.anns[caption_ids[0]]['caption']
+        # caption = self.gpt2_tokenizer(caption, return_tensors="pt")
         
         return image, caption
 
@@ -109,12 +115,12 @@ val_image_dir = "../data/val2014"
 val_captions_file = "../data/annotations/captions_val2014.json"
 val_dataset = CocoDataset(val_image_dir, val_captions_file, transform=image_transform)
 
-# Create train and validation data loaders
+
 batch_size = 32
 train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_data_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
 
-# Obtain two random samples from the training dataset
+
 samples = np.random.choice(len(train_dataset), size=2, replace=False)
 image1, caption_inputs1 = train_dataset[samples[0]]
 image2, caption_inputs2 = train_dataset[samples[1]]
@@ -127,7 +133,6 @@ image2, caption_inputs2 = train_dataset[samples[1]]
 image1 = image1.permute(1, 2, 0).numpy()
 image2 = image2.permute(1, 2, 0).numpy()
 
-# # Decode the tokenized captions
 # captions1 = [train_dataset.tokenizer.decode(token_ids) for token_ids in caption_inputs1.input_ids]
 # # captions2 = [train_dataset.tokenizer.decode(token_ids) for token_ids in caption_inputs2.input_ids]
 
@@ -156,18 +161,19 @@ criterion = nn.CrossEntropyLoss()
 num_epochs = 1000
 
 
-# # Step 7: Perform inference
 
 # Main code
 caption_generator.train()
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+tokenizer.add_special_tokens({'pad_token': tokenizer.eos_token})
 
 # Training loop
 for epoch in range(num_epochs):
     for images, captions in train_data_loader:
         optimizer.zero_grad()
-        # print(captions)
-        outputs = caption_generator(images, captions)
-        loss = criterion(outputs, captions)
+        caption_tokens = [tokenizer(caption, return_tensors="pt") for caption in captions ]
+        outputs = caption_generator(images, caption_tokens, tokenizer)
+        loss = criterion(outputs, caption_tokens)
         print(loss, "WASA")
         loss.backward()
         optimizer.step()
